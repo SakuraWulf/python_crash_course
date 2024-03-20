20e6cc5e583b6f91569ea8a718a796b6dac08ca6
SakuraWulf <156924182+SakuraWulf@users.noreply.github.com>
2024-03-20 16:48:40 -0500

    Add files via upload

---
+++ 4.4_one_million.py
@@ -0,0 +1,1 @@
+# Make a list of the numbers one to one million, and then use a for loop to print the numbers. (If the output is taking too long stop it by pressing CTRL+C or by closing the output window.)
---
+++ 4.9_cube_comprehension.py
@@ -0,0 +1,1 @@
+# Use a list comprehension to generate a list of the first 10 cubes.
