cffb00d42d6a3b90eee4fcc044c63c5a7362636c
SakuraWulf <sakurawulf@gmail.com>
2024-03-27 20:28:32 -0500

    unfinished

---
+++ 5.5_alien_colors_#3.py
@@ -0,0 +1,12 @@
+"""
+Turn your if-else chain from exercise 5.4 into an ef-elif-else chain.
+
+If the alien is green, print a message that the player earned 5 points.
+
+If the alien is yellow, print a message that the player earned 10.
+
+If the alien is red, print a message that the player earned 15.
+
+Write three versions of this program, making sure each message is
+printed for the appropriate color alien.
+"""
