518f8344b809b830248a1819aa33cb4427a26d9b
SakuraWulf <sakurawulf@gmail.com>
2024-04-04 14:11:18 -0500

    new

---
+++ 6.01_person.py
@@ -0,0 +1,16 @@
+"""
+Use a dictionary to store information about a person you know. 
+Store their first name, last name, age, and the city in which they live.
+You should have keys such as first_name, last_name, age, and city. 
+Print each piece of information stored in your dictionary."""
+
+weeknd = {'first_name': 'abel',
+          'last_name': 'tesfaye',
+          'age': 34,
+          'city': 'ontario'}
+
+print(weeknd['first_name'])
+print(weeknd['last_name'])
+print(weeknd['age'])
+print(weeknd['city'])
+
